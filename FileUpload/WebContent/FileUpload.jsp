cf0e1287e5f6bd32f200c69904a6bc014372832e
premkama98 <kamalosipalli@gmail.com>
2019-09-04 12:18:19 +0530

    commit

--- FileUpload/WebContent/FileUpload.jsp
+++ FileUpload/WebContent/FileUpload.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,10 +8,9 @@
 </head>
 <body>
 	<div align="center">
-	<form method="post" action="fileUpload" enctype="multipart/form-data">
-			Select file to upload: <input type="file" name="uploadFile" />
-			<br/><br/> 
-			<input type="submit" value="Upload" />
+		<form method="post" action="fileUpload" enctype="multipart/form-data">
+			Select file to upload: <input type="file" name="uploadFile" /> <br />
+			<br /> <input type="submit" value="Upload" />
 		</form>
 	</div>
 </body>
--- FileUpload/src/com/bluesky/training/FileUploadServlet.java
+++ FileUpload/src/com/bluesky/training/FileUploadServlet.java
@@ -2,8 +2,18 @@ package com.bluesky.training;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -26,7 +36,20 @@ public class FileUploadServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		PrintWriter writer=response.getWriter();
+		String fileName = null;
+		Connection conn;
+		Statement stmt = null;
+		ResultSet resultset;
+		try {
+			Class.forName("org.h2.Driver");
+			conn = DriverManager.getConnection("jdbc:h2:~/test", "sa", "");
+			stmt = conn.createStatement();
+		} catch (ClassNotFoundException | SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		PrintWriter writer = response.getWriter();
 		if (ServletFileUpload.isMultipartContent(request)) {
 			writer.println("File upload has been done successfully");
 			try {
@@ -34,19 +57,26 @@ public class FileUploadServlet extends HttpServlet {
 				if (formItems != null && (formItems.size()) > 0) {
 					for (FileItem item : formItems) {
 						if (!item.isFormField()) {
-							String fileName = new File(item.getName()).getName();
+							fileName = new File(item.getName()).getName();
 							item.write(new File(UPLOAD_DIRECTORY + File.separator + fileName));
 							writer.println(item.getName());
-							request.setAttribute("message", "File upload has been done successfully"+item.getName()+"");							
+							request.setAttribute("message",
+									"File Uploaded Successfully \n PATH:" + UPLOAD_DIRECTORY + "" + fileName + "");
 						}
 					}
 				}
+				stmt.execute(
+						"CREATE TABLE IF NOT EXISTS Source(STLOC_ID INT PRIMARY KEY, IDENTIFIER VARCHAR(255), PHONE VARCHAR(255),"
+								+ " FAX VARCHAR(255),ADDRESS1 VARCHAR(255), ADDRESS2 VARCHAR(255), ADDRESS3 VARCHAR(255),CITY VARCHAR(255),"
+								+ " STATE VARCHAR(255), COUNTRY VARCHAR(255),ZIPCODE VARCHAR(255),ACTIVE VARCHAR(255)) "
+								+ "AS SELECT * FROM CSVREAD('" + UPLOAD_DIRECTORY + "/" + fileName + "')");
+				resultset = stmt.executeQuery("select * from Source");
 			} catch (Exception e) {
 
 				e.printStackTrace();
 			}
 		} else {
-			
+
 			request.setAttribute("message", "Sorry this Servlet only handles file upload request");
 		}
 		getServletContext().getRequestDispatcher("/FileUploadMessage.jsp").forward(request, response);
